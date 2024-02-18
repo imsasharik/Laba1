f37bf0d8a3bf4ae8208d76c1364fb4a419c1ea42
imsasharik <94919869+imsasharik@users.noreply.github.com>
2024-02-18 18:37:02 +0200

    первый коммит
    урааа

---
+++ Source1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	short sh = 327; // -2^15...2^15-1
+	int in = 213213; // -2^31...2^31-1
+	long l = 237648; // -2^31...2^31-1
+	float fl = -10.5; // -2^31...2^31-1
+	double d = 13.234; // -2^63...2^63-1
+
+	float r;
+	float Pi = 3.14;
+	std::cout << "Радиус:";
+	std::cin >> r;
+	sq = Pi * r * r;
+
+} 
